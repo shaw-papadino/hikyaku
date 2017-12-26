8cd835d3658039f52999fbac0127e47977dcecc7
岡山将也 <okayamashoya@macbook-pro.local>
2017-12-26 18:29:05 +0900

    Initial Commit

---
+++ Hikyaku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hikyaku
+//
+//  Created by 岡山将也 on 2017/12/26.
+//  Copyright © 2017年 shouya.okayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
